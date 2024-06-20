b1c3b2496ad589e69e426426c2a85c3b6f37341f
AlenaZabolotskykh <alenka.polishchuk@gmail.com>
2024-06-20 13:01:20 +0000

    Deploying to gh-pages from @ AlenaZabolotskykh/goit-js-hw-09@5cf96b9309b514db146929a0dd6609817906e2e2 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";const a=document.querySelector(".feedback-form");a.addEventListener("submit",o);function o(e){e.preventDefault(),console.dir(e.currentTarget.elements);const{email:t,message:n}=e.currentTarget.elements,r={email:t.value,message:n.value};console.log(r)}
+//# sourceMappingURL=commonHelpers2.js.map
